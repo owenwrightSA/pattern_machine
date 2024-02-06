0495f6d56f5292d73e65a96d69cc88a8a276bf7b
Owen Wright <owenwright@seattleacademy.org>
2024-02-06 10:16:47 -0800

    checker pattern

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ const columns = document.getElementById('columns');
 const color = document.getElementById('color');
 const button = document.getElementById('button');
 const grid = document.getElementById('grid')
+const checker = document.getElementById('checker')
 
 button.addEventListener("click", makeGrid);
+checker.addEventListener("click", makeGrid2)
 
 function makeGrid() {
     console.log(color.value)
@@ -28,4 +30,46 @@ function changeColor(event) {
     event.target.style.backgroundColor = color.value
 
 
+}
+
+var Occelate = "white";
+
+function makeGrid2() {
+    console.log(color.value)
+    grid.innerHTML = "";
+    for (let i = 0; i < rows.value; i++) {
+        if (i%2 == 0){
+            Occelate = "white";
+        }
+        else {
+            Occelate = "black";
+        }
+        let rowDiv = document.createElement('div')
+        rowDiv.classList.add('row');
+        grid.appendChild(rowDiv);
+        for (let j=0; j < columns.value; j++) {
+            let box = document.createElement('div');
+            if (Occelate == "white") {
+                Occelate = "black";
+
+            }
+            else {
+                Occelate = "white";
+                console.log ('c');
+            }
+
+            if (Occelate == "white"){
+                box.style.backgroundColor = 'white';
+            }
+            else {
+                box.style.backgroundColor = "black";
+                console.log ('c');
+            }
+            
+            box.classList.add('box');
+            rowDiv.appendChild(box);
+            box.addEventListener("click", changeColor);
+
+        }
+    }
 }
